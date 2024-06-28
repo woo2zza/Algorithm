ed1b437da781b68566f7222429bbe4e85d3c969e
woo2zza <139437129+woo2zza@users.noreply.github.com>
2024-06-29 02:21:44 +0900

    [Bronze IV] Title: 주사위 세개, Time: 96 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.js
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.js
@@ -5,12 +5,14 @@ const first = parseInt(input[0]);
 const second = parseInt(input[1]);
 const third = parseInt(input[2]);
 
-if (first === second && first === third) {
+if (first == second && first == third && second == third) {
   console.log(10000 + first * 1000);
-} else if (first === second || first === third) {
+} else if (first == second) {
   console.log(1000 + first * 100);
-} else if (second === third) {
+} else if (second == third) {
   console.log(1000 + second * 100);
-} else {
+} else if (first == third) {
+  console.log(1000 + first * 100);
+} else if (first !== second && second !== first && second !== third) {
   console.log(Math.max(first, second, third) * 100);
 }
