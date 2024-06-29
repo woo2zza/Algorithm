ee150a53bff4c810557a892d9c77959baaf9a6d4
woo2zza <139437129+woo2zza@users.noreply.github.com>
2024-06-30 02:39:01 +0900

    [Bronze V] Title: A+B - 7, Time: 152 ms, Memory: 9516 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.js
@@ -0,0 +1,7 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
+const number = parseInt(input[0]);
+
+for (let count = 1; count < number + 1; count++) {
+  const [A, B] = input[count].split(" ").map(Number);
+  console.log(`Case #${count}: ${A + B}`);
+}
