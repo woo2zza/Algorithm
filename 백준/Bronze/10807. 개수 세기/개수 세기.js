d1a0214c21c89583f29d3a70e7bc235bfbb15a85
woo2zza <139437129+woo2zza@users.noreply.github.com>
2024-07-01 01:19:58 +0900

    [Bronze V] Title: 개수 세기, Time: 116 ms, Memory: 9344 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.js
+++ 백준/Bronze/10807. 개수 세기/개수 세기.js
@@ -1,10 +1,6 @@
 const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
-const count = parseInt(input[0]);
-const arr = input[1].split(" ");
-const find = parseInt(input[2]);
+const [n, arr, v] = input;
 
-let result = 0;
-for (let i = 0; i < count + 1; i++) {
-  if (parseInt(arr[i]) == find) result += 1;
-}
-console.log(result);
+const count = arr.split(" ").filter((num) => num === v).length;
+
+console.log(count);
