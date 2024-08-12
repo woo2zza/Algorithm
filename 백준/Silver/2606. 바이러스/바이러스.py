e097104984a44500aeca26c0b941bc2145c5282c
woo2zza <139437129+woo2zza@users.noreply.github.com>
2024-08-13 01:40:37 +0900

    [Silver III] Title: 바이러스, Time: 60 ms, Memory: 34052 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,24 +1,24 @@
+
 from collections import deque
 
-C = int(input())
-N = int(input())
-arr = [list(map(int, input().split())) for _ in range(N)]
+Com = int(input())
+Net = int(input())
+arr = [list(map(int, input().split(' '))) for _ in range(Net)]
+
 q = deque()
 q.append(1)
-cnt = -1
-used = [0] * (C+1)
+used = [0] * (Com + 1)
 used[1] = 1
-
 while q:
     new = q.popleft()
-    cnt += 1
-    for i in range(N):
-        if arr[i][0] == new:
-            if used[arr[i][1]] != 1:
-                used[arr[i][1]] = 1
-                q.append(arr[i][1])
+    for i in range(Net):
         if arr[i][1] == new:
-            if used[arr[i][0]] != 1:
+            if used[arr[i][0]] != 1: 
                 used[arr[i][0]] = 1
                 q.append(arr[i][0])
-print(cnt)
+        if arr[i][0] == new:
+            if used[arr[i][1]] != 1: 
+                used[arr[i][1]] = 1
+                q.append(arr[i][1])
+print(sum(used)-1)
+
