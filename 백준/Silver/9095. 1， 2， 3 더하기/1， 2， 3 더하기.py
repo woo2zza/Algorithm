8046ba5d4beeeeaccfbab7a2a97ec269d0b6b81a
woo2zza <139437129+woo2zza@users.noreply.github.com>
2024-09-29 20:20:39 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -0,0 +1,15 @@
+import sys
+
+arr = [0] * 11
+arr[1] = 1
+arr[2] = 2
+arr[3] = 4
+T = int(input())
+
+for i in range(4,11):
+    arr[i] = arr[i-1] + arr[i-2] + arr[i-3]
+
+for _ in range(T):
+    n = int(input())
+    print(arr[n])
+
