d0f38517839ed1c1a52c49280e555a9299fe0215
woo2zza <139437129+woo2zza@users.noreply.github.com>
2024-11-19 00:34:15 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.40 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
@@ -1,15 +1,3 @@
 def solution(A,B):
-    answer = 0
-    lst_A = []
-    lst_B = []
-    for i in A:
-        lst_A.append(int(i))
-    lst_A.sort()
-    for i in B:
-        lst_B.append(int(i))
-    lst_A.sort()
-    lst_B.sort(reverse = True)
-    for i in range(len(A)):
-        result = lst_A[i] * lst_B[i]
-        answer += result
+    answer = sum([a * b for a, b in zip(sorted(A), sorted(B, reverse = True))])
     return answer
