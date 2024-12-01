4b4d0db1a850ef88c80fb25f8dba59ef29cb1027
woo2zza <139437129+woo2zza@users.noreply.github.com>
2024-12-01 20:32:38 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,7 +1,6 @@
 def solution(n):
-    result = list(bin(n)[2:])
-    cnt = result.count('1')
-    for i in range(n+1, n+100):
-        if list(bin(i)[2:]).count('1') == cnt:
-            return i
-  
+    answer = n + 1
+    zero = bin(n)[2:].count('1')
+    while(zero != bin(answer)[2:].count('1')):
+        answer += 1
+    return answer
