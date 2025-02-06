37034615bd705b5e9cde7e18e2e287790e671fe9
woo2zza <139437129+woo2zza@users.noreply.github.com>
2025-02-06 22:42:05 +0900

    [level 2] Title: 카펫, Time: 0.22 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -1,14 +1,13 @@
 function solution(brown, yellow) {
-    const answer = [];
-    let height = 2;
-    let width = brown / 2;
-    let result = -1;
+    let width = brown / 2 
+    let height = 2
+    let result = -1
     
-    while(result !== 0){
-        height++;
-        width--;
-        let mul = height * width;
-        result = mul - brown - yellow;
+    while (result !== 0) {
+        width--
+        height++
+        let mul = width * height
+        result = mul - brown - yellow
     }
-    return [ width , height ];
+    return [width, height]
 }
